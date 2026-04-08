20f4c3c8d59bf1574b37240031e803622a77f0a1
Allan-O-H <allan.oh98@gmail.com>
2026-04-08 11:48:50 -0400

    Add AWS provider and VPC configuration for dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,3 +11,22 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ========================================
+# Networking - VPC & cidr range
+# ========================================
+# Create a dev VPC with specified CIDR range
+resource "aws_vpc" "dev-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name        = "dev"
+    enviornment = "dev"
+  }
+}
