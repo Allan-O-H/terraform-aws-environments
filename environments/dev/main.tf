baa3538020e0356426e28f2b20cb52604943623f
Allan-O-H <allan.oh98@gmail.com>
2026-05-01 09:39:48 -0400

    Refactor VPC and subnet resource names for consistency; add internet gateway and route table for dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,9 +18,8 @@ provider "aws" {
 # ========================================================
 # Networking - VPC, Subnets, NAT Gateway, Internet Gateway
 # ========================================================
-
 ## DEV VPC 
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "dev_vpc" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
@@ -33,34 +32,62 @@ resource "aws_vpc" "dev-vpc" {
 }
 
 # Private Subnets
-resource "aws_subnet" "dev-private-1" {
+resource "aws_subnet" "dev_private_1" {
   vpc_id                  = aws_vpc.dev-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1"
   map_public_ip_on_launch = false
 
   tags = {
-    Name        = "dev-private-1"
+    Name        = "dev_private_1"
     environment = "dev"
   }
 }
 
 # Public Subnets
-resource "aws_subnet" "dev-public-1" {
+resource "aws_subnet" "dev_public_1" {
   vpc_id                  = aws_vpc.dev-vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1"
   map_public_ip_on_launch = true
 
   tags = {
-    Name        = "dev-public-1"
+    Name        = "dev_public_1"
     environment = "dev"
   }
 }
 
+resource "aws_internet_gateway" "dev_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "dev_igw"
+    environment = "dev"
+  }
+}
+
+resource "aws_route_table" "dev_public_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_igw.id
+  }
+
+  tags = {
+    Name        = "dev_public_rt"
+    environment = "dev"
+  }
+}
+
+resource "aws_rout_table_association" "dev_public_rt_a" {
+  subnet_id = aws_subnet.dev_public_1.id
+  route_table_id = aws_route_table.dev_public_rt.id
+}
+
 # ========================================================
 ## Prod VPC
-resource "aws_vpc" "prod-vpc" {
+resource "aws_vpc" "prod_vpc" {
   cidr_block           = "10.1.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
@@ -73,27 +100,29 @@ resource "aws_vpc" "prod-vpc" {
 }
 
 # Private Subnets
-resource "aws_subnet" "prod-private-1" {
+resource "aws_subnet" "prod_private_1" {
   vpc_id                  = aws_vpc.prod-vpc.id
   cidr_block              = "10.1.1.0/24"
   availability_zone       = "us-east-1"
   map_public_ip_on_launch = false
 
   tags = {
-    Name        = "prod-private-1"
+    Name        = "prod_private_1"
     environment = "prod"
   }
 }
 
 # Public Subnets
-resource "aws_subnet" "prod-public-1" {
+resource "aws_subnet" "prod_public_1" {
   vpc_id                  = aws_vpc.prod-vpc.id
   cidr_block              = "10.1.2.0/24"
   availability_zone       = "us-east-1"
   map_public_ip_on_launch = true
 
   tags = {
-    Name        = "prod-public-1"
+    Name        = "prod_public_1"
     environment = "prod"
   }
 }
+
+
