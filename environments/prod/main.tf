9af1b8529042057650646490a87000b52dc0bfdd
Allan <allan.oh98@gmail.com>
2026-05-04 20:40:08 -0400

    Fix header formatting and correct provider description in PROD environment configuration

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,12 +1,57 @@
 # ========================================
-# Terraform & Provider Configuration for Prod Environment
+# Terraform & Provider Configuration for PROD Environment
 # ========================================
-# Configure required Terraform version and Azure provider (aws)
+# Configure required Terraform version and AWS provider 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"  
+}
+
+# ========================================================
+# Networking - PROD VPC
+# ========================================================
+resource "aws_vpc" "prod_vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "prod"
+    enviornment = "prod"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "prod_private_1" {
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = "10.1.1.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "prod_private_1"
+    environment = "prod"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "prod_public_1" {
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = "10.1.2.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "prod_public_1"
+    environment = "prod"
   }
 }
